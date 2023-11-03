d91ac269e43369c56216b67df1ff465b6fd9fc40
shardv07 <sharad.vidyapathy@gmail.com>
2023-11-03 14:35:00 -0400

    updated to fix attribute issue

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@
 
 data "tfe_outputs" "resource_grp" {
   organization = "MasonClouds"
-  workspace    = "sysengg-dev-us-east"
+  workspace    = "storage-dev-us-east"
 }
 
 resource "azurerm_storage_account" "acme_sa" {
   name                     = var.storage_acc_name
-  resource_group_name      = data.tfe_outputs.resource_grp.resource_group_name
-  location                 = data.tfe_outputs.resource_grp.resource_group_location
+  resource_group_name      = data.tfe_outputs.resource_grp.values.resource_group_name
+  location                 = data.tfe_outputs.resource_grp.values.resource_group_location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
